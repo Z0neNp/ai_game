98bcb50ef8adf1622c716c624f2395040cfb551a
Maxim Chanturiay <ardorm79@hotmail.co.il>
2020-11-12 19:09:14 +0200

    refactored

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.api_1_0_0 import *
+from src.api_1_1_0 import *
 from src.api_graphical_1_0_0 import drawMaze
 
 if __name__ == "__main__":
@@ -11,11 +11,8 @@ if __name__ == "__main__":
   Logger().info("Game", "Maze is ready")
   iterations = config["iterations_limit"]
   while (iterations > 0):
-    i = 100000
-    while i > 0:
-      i = i - 1
     iteration_id = iterations
-    # Logger().debug("Game", f"---> Iteration {str(iteration_id)} started")
+    Logger().debug("Game", f"---> Iteration {str(iteration_id)} started")
     print(str(maze))
     
     if (len(maze.teams) < 1):
@@ -29,9 +26,9 @@ if __name__ == "__main__":
 
     for team in maze.teams:
       nextMove(team)
-      # Logger().debug("Game", f"Team {str(team.id)} has made a move")
+      Logger().debug("Game", f"Team {str(team.id)} has made a move")
 
-    # Logger().debug("Game", f"Iteration {iteration_id} Ended <---")
+    Logger().debug("Game", f"Iteration {iteration_id} Ended <---")
     iterations = iteration_id - 1
 
   Logger().info("Game", "The game has ended with a draw")
