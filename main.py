3acf994492abdcc46c6d713f58b8e16f5a7e4814
Maxim Chanturiay <ardorm79@hotmail.co.il>
2020-11-13 12:48:17 +0200

    refactored

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
       Logger().info("Game", "The game has ended with a draw")
       exit(0)
 
-    # if (len(maze.teams) < 2):
-    #   winner = maze.teams[0]
-    #   Logger().info("Game", f"The winner is team {str(winner.id)}")
-    #   exit(0)
+    if (len(maze.teams) < 2):
+      winner = maze.teams[0]
+      Logger().info("Game", f"The winner is team {str(winner.id)}")
+      exit(0)
 
     for team in maze.teams:
       nextMove(team)
